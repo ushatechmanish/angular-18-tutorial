1ade9e3ccd8e41b110c2b1101f0c545f827ceaef
Manish Chauhan <manish.chauhan@ushatech.in>
2024-08-11 18:53:00 +0530

    Enable routerLink instead of href When you click on the links in the navigation now, you should not see any blinking and only the content of the page itself (i.e., router-outlet) being changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink,RouterOutlet } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { ChildComponent } from "./child/child.component";
 import { CommentsComponent } from "./comments/comments.component";
@@ -7,12 +7,12 @@ import { CommentsComponent } from "./comments/comments.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserComponent, ChildComponent, CommentsComponent],
+  imports: [RouterLink, RouterOutlet, UserComponent, ChildComponent, CommentsComponent],
   template: `
   <nav>
-      <a href="/">Child</a>
+      <a routerLink="/">Child</a>
       |
-      <a href="/user">User</a>
+      <a routerLink="/user">User</a>
     </nav>
     <router-outlet/>
   `,
