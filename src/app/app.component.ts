4e3927c17e4ee6ff8816f458258099cee0709487
Manish Chauhan <manish.chauhan@ushatech.in>
2024-08-11 15:16:41 +0530

    We can use interpolation inside backticks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   template:`
-  Hello {{ city }}
+  Hello {{ city }} , {{ 1+1 }}
   `,
   styles:
   `
