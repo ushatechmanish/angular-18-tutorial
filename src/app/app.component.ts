8ef8bb635237dc2d828ba6b259b00ed8e5e9739a
Manish Chauhan <manish.chauhan@ushatech.in>
2024-08-11 15:34:32 +0530

    Control Flow in Components - `@for` for users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,8 @@ import { UserComponent } from './user/user.component';
   standalone: true,
   imports: [RouterOutlet, UserComponent],
   template: `
-    @for (item of operatingSystems; track item.id) {
-      {{item.name}}
+    @for (user of users; track user.id) {
+      <p>{{user.name}}</p>
     }
   `,
   styles:
@@ -20,6 +20,5 @@ import { UserComponent } from './user/user.component';
 })
 export class AppComponent {
 
-  operatingSystems = [{ id: 'win', name: 'Windows' }, { id: 'osx', name: 'MacOS' }, { id: 'linux', name: 'Linux' }];
-
+  users = [{id: 0, name: 'Sarah'}, {id: 1, name: 'Amy'}, {id: 2, name: 'Rachel'}, {id: 3, name: 'Jessica'}, {id: 4, name: 'Poornima'}];
 }
