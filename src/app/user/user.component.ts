607ac87502bce95378c4ef650889a1f620a6f476
Manish Chauhan <manish.chauhan@ushatech.in>
2024-08-11 17:35:00 +0530

    Optimizing images

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,13 +1,29 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
-  template: `Hello {{ username }} `,
+  imports: [NgOptimizedImage],
+  template: `
+  <p>Username: {{ username }}</p>
+    <p>Preferred Framework:</p>
+    <ul>
+      <li>
+        Static Image:
+        <img ngSrc="logo.png" alt="Angular logo" width="32" height="32" />
+      </li>
+      <li>
+        Dynamic Image:
+        <img [ngSrc]="logoUrl" [alt]="logoAlt" width="32" height="32" />
+      </li>
+    </ul>
+  `,
   styleUrl: './user.component.css'
 })
 export class UserComponent 
 {
-  @Input() username: string = '';
+  logoUrl = 'logo.png';
+  logoAlt = 'Angular logo';
+  username = 'youngTech';
 }
